646fec4168e1a7a666a5d9dd9edd16dad60d6f7c
hillary <hillaryseby@gmail.com>
2024-05-21 23:27:53 -0400

    UserName added to the dropdown in navbar after loggin in

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Navroutes } from '../constants/navroutes';
 import { AuthServiceService } from '../services/auth-service.service';
 import { UntypedFormBuilder } from '@angular/forms';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-nav-bar',
@@ -14,11 +14,15 @@ export class NavBarComponent implements OnInit {
   routeToBuyProperty : string = Navroutes.buyProperty;
   items: MenuItem[] | undefined;
 constructor(private route :Router,
-            private authService :AuthServiceService
+            private authService :AuthServiceService,
+            private messageService : MessageService
 ){}
 
 ngOnInit(): void {
   this.items = [
+    {
+      label : this.UserName
+    },
     {
         label : "Profile",
         icon : "pi pi-user"
@@ -26,11 +30,24 @@ ngOnInit(): void {
     {
       label: "Account Settings",
       icon : "pi pi-crown"
+    },
+    {
+      label : "Logout",
+      icon : "pi pi-sign-out",
+      command :  () => {
+        this.redirectToLogin("logout");
+      } 
     }
 ];
 }
 onRegister(){
   this.route.navigate([Navroutes.userRegistration])
+}
+
+get UserName() {
+  if(localStorage.getItem('user')) {
+    return JSON.parse(localStorage.getItem('user')).userName;
+  }
 }
 
 async redirectToLogin(action:string){
@@ -39,7 +56,11 @@ async redirectToLogin(action:string){
   }
   else{
    await this.authService.isLoggedOut();
-   this.route.navigate([Navroutes.loginPage]);
+   this.messageService.add({severity : 'success', detail : 'Logged Out Successfully'})
+   setTimeout(()=>{
+    this.route.navigate([Navroutes.loginPage]);
+   },2000)
+  
   }
   
 }
